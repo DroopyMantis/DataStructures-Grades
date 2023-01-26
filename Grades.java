5a2c8a39eb99f76eea4ce43b0ea9433fec5b0514
DroopyMantis <95454905+DroopyMantis@users.noreply.github.com>
2023-01-26 01:37:46 -0800

    Update Grades.java

--- Grades.java
+++ Grades.java
@@ -1,131 +1,108 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Grades {
+class Grades {
 
     private int[] arr;
+    private static int totalGrades;
 
-    public Grades(){
+    public Grades() {
         arr = new int[0];
     }
 
-    public Grades(int n){
-        arr = new int[n];
+    public Grades(int[] myArr) {
+        arr = myArr;
     }
 
-    public void setValues(int[] myArr){
-        for(int x: arr){
-            arr[x] = myArr[x];
-        }
+    public void setValues(int[] myArr) {
+        arr = myArr;
     }
 
-    public int[] getValues(){
+    public int[] getValues() {
         return arr;
     }
 
-    public static void main(String[] args) {
-        Scanner myObj = new Scanner(System.in);
-        System.out.println("Enter the number of grades to input");
-
-        String totalGradesStr = myObj.nextLine();
-        int totalGrades = Integer.parseInt(totalGradesStr);
-
-        Grades gradeBook = new Grades(totalGrades);
-
-        for (int i = 0; i < totalGrades; i++) {
-            System.out.println("Enter grade #" + (i + 1));
-            int grade = myObj.nextInt();
-
-            gradeBook.arr[i] = grade;
-        }
-        System.out.println("Highest = " + gradeBook.getHighest());
-        System.out.println("Lowest = " + gradeBook.getLowest());
-        System.out.println("Total grades = " + gradeBook.getNumberOfGrades());
-        System.out.println("Grade average = " + gradeBook.getAvg());
-        System.out.println("Failed grades = " + gradeBook.failing(60));
-        gradeBook.histogram();
-    }
-
-    public int getHighest(){
+    public int getHighest() {
         int highest = 0;
-        for(int x : arr){
-            if(x > highest){
+        for (int x : arr) {
+            if (x > highest) {
                 highest = x;
             }
         }
         return highest;
     }
 
-    public int getLowest(){
+    public int getLowest() {
         int lowest = arr[0];
-        for(int x : arr){
-            if(x < lowest){
+        for (int x : arr) {
+            if (x < lowest) {
                 lowest = x;
             }
         }
         return lowest;
     }
 
-    public int getNumberOfGrades(){
+    public int numOfGrades() {
         return arr.length;
     }
 
-    public double getAvg(){
+    public double getAvg() {
         double sum = 0.0;
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             sum += arr[i];
         }
-        double avg = (sum/arr.length)/1.0;
+        double avg = (sum / arr.length) / 1.0;
         return avg;
     }
 
-    public int failing(int n){
+    public int failing(int gradeValue) {
         int failed = 0;
-        for(int x : arr){
-            if(x < n){
+        for (int x : arr) {
+            if (x < gradeValue) {
                 failed++;
             }
         }
         return failed;
     }
 
-    public void histogram(){
-        int a = 0; int b = 0; int c = 0; int d = 0; int f = 0;
-        for(int x : arr){
-            if(x < 60){
+    public void histogram() {
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        int d = 0;
+        int f = 0;
+        for (int x : arr) {
+            if (x < 60) {
                 f++;
-            }
-            else if(x < 70){
+            } else if (x < 70) {
                 d++;
-            }
-            else if(x < 80){
+            } else if (x < 80) {
                 c++;
-            }
-            else if(x < 90){
+            } else if (x < 90) {
                 b++;
-            }
-            else{
+            } else {
                 a++;
             }
         }
         String star = "*";
         String A = "";
-        for(int i = 0; i < a; i++){
+        for (int i = 0; i < a; i++) {
             A = A + star;
         }
         String B = "";
-        for(int i = 0; i < b; i++){
+        for (int i = 0; i < b; i++) {
             B = B + star;
         }
         String C = "";
-        for(int i = 0; i < c; i++){
+        for (int i = 0; i < c; i++) {
             C = C + star;
         }
         String D = "";
-        for(int i = 0; i < d; i++){
+        for (int i = 0; i < d; i++) {
             D = D + star;
         }
         String F = "";
-        for(int i = 0; i < f; i++){
+        for (int i = 0; i < f; i++) {
             F = F + star;
         }
         System.out.println("90 - 100 | " + A);
@@ -135,5 +112,55 @@ public class Grades {
         System.out.println("< 60 | " + F);
     }
 
+    public static void scan() {
 
+        try {
+            Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter the number of grades to input");
+
+            totalGrades = myObj.nextInt();
+            if(totalGrades < 0){
+                System.out.println("Please enter a positive number");
+                scan();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a positive number");
+            scan();
+        }
+    }
+
+    public static int checkGrade(){
+        int grade = 0;
+        try{
+            Scanner myObj = new Scanner(System.in);
+            grade = myObj.nextInt();
+            if( grade > 100 || grade < 0){
+                System.out.println("Please enter a number from 0 - 100");
+                checkGrade();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please enter a number from 0 - 100");
+            checkGrade();
+        }
+        return grade;
+    }
+
+    public static void main(String[] args) {
+        scan();
+
+        int[] myArr = new int[totalGrades];
+        for (int i = 0; i < myArr.length; i++) {
+            System.out.println("Enter grade #" + (i + 1));
+            myArr[i] = checkGrade();
+        }
+
+        Grades gradeBook = new Grades(myArr);
+
+        System.out.println("Highest = " + gradeBook.getHighest());
+        System.out.println("Lowest = " + gradeBook.getLowest());
+        System.out.println("Total grades = " + gradeBook.numOfGrades());
+        System.out.println("Grade average = " + gradeBook.getAvg());
+        System.out.println("Failed grades = " + gradeBook.failing(60));
+        gradeBook.histogram();
+    }
 }
